9d7e366f02351e6546930b9d5225fefc25153a33
Christos Pappas <chrispappas99@yahoo.gr>
2020-04-13 22:06:32 +0300

    Add project skeleton

--- src/pizza1.c
+++ src/pizza1.c
@@ -1,34 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #ifdef WINDOWS_VS
 #define HAVE_STRUCT_TIMESPEC // https://stackoverflow.com/questions/33557506/timespec-redefinition-error
 #endif // WINDOWS_VS
-
 #include <pthread.h>
 
-#define NUM_THREADS	5
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
-void *say_hello(void *tid)
+void wait(int seconds)
 {
-	int id = *(int *)tid;
-	printf("Hello from thread %d\n", id);
-	free(tid);
+#ifdef _WIN32
+	Sleep(1000 * seconds);
+#else
+	sleep(seconds);
+#endif
+}
+
+#define N 10
+
+pthread_mutex_t lock;
+pthread_cond_t  cond;
+
+int cooks = 2; //diathesimoi paraskevastes
+int id[N];
+
+void *order(void *x) {
+
+	int id = *(int *)x;
+	int rc;
+	printf("Hello from order: %d\n", id);
+
+	rc = pthread_mutex_lock(&lock);
+	while (cooks == 0) {
+		printf("H paraggelia %d, den brike diathesimo paraskevasti. Blocked...\n", id);
+		rc = pthread_cond_wait(&cond, &lock);
+	}
+	printf("H paraggelia %d eksipiretitai.\n", id);
+	cooks--;
+	rc = pthread_mutex_unlock(&lock);
+
+	wait(2); //kane kapoia douleia me ton paraskevasti
+
+	rc = pthread_mutex_lock(&lock);
+	printf("H paraggelia %d eksipiretithike epitixos! \n", id);
+	cooks++;
+	rc = pthread_cond_signal(&cond);
+	rc = pthread_mutex_unlock(&lock);
+
 	pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[])
-{
-	pthread_t threads[NUM_THREADS];
-	for (int t = 0; t < NUM_THREADS; t++) {
-		int *id = malloc(sizeof(int));
-		*id = t + 1;
-		if (pthread_create(&threads[t], NULL, &say_hello, id)) {
-			printf("Thread creation failed");
-			exit(-1);
-		}
+
+int main() {
+
+	int rc;
+	pthread_t threads[N];
+
+	pthread_mutex_init(&lock, NULL);
+	pthread_cond_init(&cond, NULL);
+
+	for (int i = 0; i < N; i++) {
+		id[i] = i + 1;
+		printf("Main: creating thread %d\n", i + 1);
+		rc = pthread_create(&threads[i], NULL, order, &id[i]);
 	}
-	for (int t = 0; t < NUM_THREADS; t++) {
-		pthread_join(threads[t], NULL);
+
+	for (int i = 0; i < N; i++) {
+		pthread_join(threads[i], NULL);
 	}
-	pthread_exit(NULL);
+
+	pthread_mutex_destroy(&lock);
+	pthread_cond_destroy(&cond);
+
+	return 0;
 }
