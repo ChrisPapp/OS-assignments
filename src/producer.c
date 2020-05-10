eb5d2f8c420830552186f421850183a204a0528e
pvarouktsis <p.varouktsis@gmail.com>
2020-05-10 13:42:00 +0300

    Implement cooling time of pizzas in producer

--- src/producer.c
+++ src/producer.c
@@ -9,9 +9,17 @@ void producer_init(struct producer *pd, struct theme *theme, int resource_1, int
 	pd->th = theme;
 	pd->sum_time = 0;
 	pd->max_time = 0;
+	pd->short_sum_time = 0;
+	pd->short_max_time = 0;
 	resource_init(&pd->res_1, resource_1);
 	resource_init(&pd->res_2, resource_2);
 	resource_init(&pd->res_3, resource_3);
+  if (pthread_mutex_init(&pd->short_max_time_mutex, NULL) != 0) {
+		printf("Error in short_max_time_mutex initialization.");
+	}
+  if (pthread_mutex_init(&pd->short_sum_time_mutex, NULL) != 0) {
+		printf("Error in short_sum_time_mutex initialization.");
+	}
   if (pthread_mutex_init(&pd->max_time_mutex, NULL) != 0) {
 		printf("Error in max_time_mutex initialization.");
 	}
@@ -24,50 +32,72 @@ void producer_destroy(struct producer *pd) {
 	resource_destroy(&pd->res_1);
 	resource_destroy(&pd->res_2);
 	resource_destroy(&pd->res_3);
-  pthread_mutex_destroy(&pd->max_time_mutex);
+  pthread_mutex_destroy(&pd->short_max_time_mutex);
+  pthread_mutex_destroy(&pd->short_max_time_mutex);
+  pthread_mutex_destroy(&pd->sum_time_mutex);
   pthread_mutex_destroy(&pd->sum_time_mutex);
 }
 
 void producer_place_request(struct producer *pd, int from_cust, int count) {
   int time_working_res_3;
 	int clock_start, clock_stop, time_passed;
+	//int short_clock_start, short_clock_stop, short_time_passed;
 
 	clock_start = get_time_passed();
   pd->th->on_request_begin(from_cust, count);
-	// reserve resource_1
+	/* reserve resource_1 */
 	resource_commit(&pd->res_1);
-	// working resource_1
+	/* working resource_1 */
 	int current_object = 0;
 	while (current_object < count) {
 		current_object++;
 		pd->th->on_res_1_assign(from_cust, current_object);
 		wait_(T_RESOURCE_1);
 	}
-	// reserve resource_2
+	/* reserve resource_2 */
 	resource_commit(&pd->res_2);
 	pd->th->on_res_2_assign(from_cust);
-	// release resource_1
+	/* release resource_1 */
 	resource_release(&pd->res_1);
-  // working resource_2
+  /* working resource_2 */
 	wait_(T_RESOURCE_2);
-	// reserve resource_3
+	//short_clock_start = get_time_passed();
+	/* reserve resource_3 */
 	resource_commit(&pd->res_3);
 	pd->th->on_res_3_assign(from_cust);
-	// release resource_2
+	/* release resource_2 */
   resource_release(&pd->res_2);
-  // working resource_3
+  /* working resource_3 */
 	time_working_res_3 = rand_generator(T_RESOURCE_3_LOW_LIMIT, T_RESOURCE_3_HIGH_LIMIT);
 	wait_(time_working_res_3);
 	clock_stop = get_time_passed();
 	time_passed = clock_stop - clock_start;
-	pd->th->on_request_complete(from_cust, time_passed);
+	//short_clock_stop = get_time_passed();
+	//short_time_passed = short_clock_stop - short_clock_start;
+	//sync_printf("%d, %d\n", short_time_passed, time_working_res_3);
+	pd->th->on_request_complete(from_cust, time_passed, time_working_res_3);
 	wait_(time_working_res_3); 
-	// release resource_3
+	/* release resource_3 */
   resource_release(&pd->res_3);
-  
-	// update time counters
+	/* update time counters */
+	producer_check_if_short_time_max(pd, time_working_res_3);
+	producer_increment_short_time(pd, time_working_res_3);
 	producer_check_if_time_max(pd, time_passed);
 	producer_increment_time(pd, time_passed);
+}
+
+void producer_increment_short_time(struct producer *pd, int time) {
+  pthread_mutex_lock(&pd->short_sum_time_mutex);
+  pd->short_sum_time += time; // summarizing short_time
+  pthread_mutex_unlock(&pd->short_sum_time_mutex);
+}
+
+void producer_check_if_short_time_max(struct producer *pd, int time) {
+  pthread_mutex_lock(&pd->short_max_time_mutex);
+	if (pd->short_max_time < time) {
+		pd->short_max_time = time;
+	}
+	pthread_mutex_unlock(&pd->short_max_time_mutex);
 }
 
 void producer_increment_time(struct producer *pd, int time) {
--- src/producer.h
+++ src/producer.h
@@ -7,6 +7,10 @@ struct producer {
   struct resource res_1;
   struct resource res_2;
   struct resource res_3;
+  unsigned int short_sum_time;
+  unsigned int short_max_time;
+  pthread_mutex_t short_sum_time_mutex;
+  pthread_mutex_t short_max_time_mutex;
   unsigned int sum_time;
   unsigned int max_time;
   pthread_mutex_t sum_time_mutex;
@@ -16,5 +20,7 @@ struct producer {
 void producer_init(struct producer *pd, struct theme *theme, int resource_1, int resource_2, int resource_3);
 void producer_destroy(struct producer *pd);
 void producer_place_request(struct producer *pd, int from, int count);
+void producer_check_if_short_time_max(struct producer *pd, int time);
+void producer_increment_short_time(struct producer *pd, int time);
 void producer_check_if_time_max(struct producer *pd, int time);
 void producer_increment_time(struct producer *pd, int time);
